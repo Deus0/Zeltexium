19a1b93d847ec4b92d9a8335d58ffc8539afb285
Marz <marz.tierney@gmail.com>
2017-12-19 20:49:17 +1100

    Settings Added

---
+++ Assets/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Zeltex.Guis
+{
+    public class Settings : MonoBehaviour
+    {
+        public AudioMixer MainMixer;
+        public AnimationCurve MixerCurve;
+
+        public void SetAudioLevel(float NewAudioLevel)
+        {
+            MainMixer.SetFloat("Master", -80 + MixerCurve.Evaluate(NewAudioLevel) * 100);
+        }
+    }
+}
